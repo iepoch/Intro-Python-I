254831413b692b85050e031a2d857996f14e6fc8
Stefan <steaclem@live.com>
2019-05-09 18:02:51 -0700

    tried a new approach not sure if this really waht should be done. Reviewed this with other work

--- src/cal.py
+++ src/cal.py
@@ -19,23 +19,40 @@ and does the following:
    Then exit the program.
 """
 
+
 import sys
 import calendar
 from datetime import datetime
 
-d = datetime.now()
+date = datetime.now()
 
-cd = input("Enter a month follwed by year ie(5 2020): ").split()
+datenow = input("You must enter a month using 1-12 [space] and year (ie. 2017): ").split()
 
-if len(cd) == 0:
-      print(calendar.month(d.year, d.month))
-elif len(cd) == 1 and cd[0].isdigit() and (int(cd[0])) <=12:
-      month = int(cd[0])
-      print(calendar.month(d.year, month))
-elif len(cd) == 2 and cd[0].isdigit() and cd[1].isdigit() \
-and(int(cd[0] <= 12)) and (len(cd[1]) ==4):
-  month = int(cd[0])
-  year = int(cd[1])
-  print(calendar.month(year, month))
+if len(datenow) == 0:
+    print(calendar.month(date.year, date.month))
+elif len(datenow) == 1 and datenow[0].isdigit() and (int(datenow[0]) <= 12):
+    month = int(datenow[0])
+    print(calendar.month(date.year, month))
+elif len(datenow) == 2 and datenow[0].isdigit() and datenow[1].isdigit() \
+        and (int(datenow[0]) <= 12) and (len(datenow[1]) == 4):
+    month = int(datenow[0])
+    year = int(datenow[1])
+    print(calendar.month(year, month))
+else:
+    print('You must enter a number between 1 and 12 for the first number. And then [space] and year format.')
+
+# number_of_days = int(input('Enter number of days in month: '))
+# first_weekday = input('First weekday of the month: ')
 
 
+# weekdays = {'Su': 0, 'M': 1, 'T': 2, 'W': 3, 'Th': 4, 'F': 5, 'Sa': 6}
+# print('{:>3}{:>4}{:>4}{:>4}{:>4}{:>4}{:>4}'
+#       .format('Su', 'M', 'T', 'W', 'Th', 'F', 'Sa'))
+
+
+# print (weekdays[first_weekday]*4*' ' + '{:>3}'.format(1), end=' ')
+# for current_day in range(1, number_of_days+1):
+#     # Second elif block
+#     if (weekdays[first_weekday] + current_day) % 7 == 0:
+#         print ()
+#     print ('{:>3}'.format(current_day ), end=' ')
