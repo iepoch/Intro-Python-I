0c9bbb3bb69c6e361bc79c38a4e55aa8e40f202b
iepoch <steaclem@gmail.com>
2019-05-13 18:06:43 -0700

    saving my slive information

--- src/slices.py
+++ src/slices.py
@@ -8,7 +8,7 @@ overview: https://stackoverflow.com/a/509295
 
 Use Python's slice syntax to achieve the following:
 """
-
+# Brackets [] it's Slices and () it's tupless
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
